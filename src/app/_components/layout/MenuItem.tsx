920a65a159102a06e6b6d1b895e47752f7c0c178
Dasom <dev.dasom1012@gmail.com>
2024-04-11 12:11:38 +0900

    refactor(MenuItem): 컴포넌트 memo

--- src/app/_components/layout/MenuItem.tsx
+++ src/app/_components/layout/MenuItem.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import Link from 'next/link';
 import clsx from 'clsx';
 import Icon from '../common/Icon';
@@ -11,19 +12,25 @@ export interface MenuItemProps {
 	mini?: boolean;
 }
 
-const MenuItem = ({ icon, path, label, active, mini }: MenuItemProps) => {
+const MenuItem = memo(function MenuItem({
+	icon,
+	path,
+	label,
+	active,
+	mini,
+}: MenuItemProps) {
 	return (
 		<Link
 			href={path}
 			className={clsx({
 				[styles.menuItem]: true,
-				[styles.active]: active,
-				[styles.mini]: mini,
+				[styles.menuItemActive]: active,
+				[styles.menuItemMini]: mini,
 			})}
 		>
 			<Icon icon={icon} type={active ? 'fill' : 'outline'} />
 			<span className={styles.label}>{label}</span>
 		</Link>
 	);
-};
+});
 export default MenuItem;
