e91cce3ecd028df6c0f2710b74cd35c7ea46f81d
Dasom <dev.dasom1012@gmail.com>
2024-04-15 19:29:19 +0900

    refactor: Toggle 상태를 MenuItem 컴포넌트에서 확인

--- src/app/_components/layout/GuideMenu.tsx
+++ src/app/_components/layout/GuideMenu.tsx
@@ -1,8 +1,5 @@
-'use client';
-import clsx from 'clsx';
 import MenuItem, { MenuItemProps } from './MenuItem';
 import styles from './styles/guide.module.scss';
-import { useToggleMenu } from './ToggleMenuProvider';
 
 const menuList: MenuItemProps[] = [
 	{ icon: 'home', label: '홈', path: '/' },
@@ -11,17 +8,11 @@ const menuList: MenuItemProps[] = [
 ];
 
 const GuideMenu = () => {
-	const { isOpen } = useToggleMenu();
 	return (
-		<ul
-			className={clsx({
-				[styles.guideMenu]: isOpen,
-				[styles.guideMenuMini]: !isOpen,
-			})}
-		>
+		<ul className={styles.guideMenu}>
 			{menuList.map((menu) => (
 				<li key={menu.path}>
-					<MenuItem {...menu} mini={!isOpen} />
+					<MenuItem {...menu} />
 				</li>
 			))}
 		</ul>
--- src/app/_components/layout/MenuItem.tsx
+++ src/app/_components/layout/MenuItem.tsx
@@ -1,15 +1,16 @@
+'use client';
 import { memo } from 'react';
 import Link from 'next/link';
 import clsx from 'clsx';
 import Icon from '../common/Icon';
 import styles from './styles/menuItem.module.scss';
+import { useToggleMenu } from './ToggleMenuProvider';
 
 export interface MenuItemProps {
 	icon: string;
 	path: string;
 	label: string;
 	active?: boolean;
-	mini?: boolean;
 }
 
 const MenuItem = memo(function MenuItem({
@@ -17,15 +18,15 @@ const MenuItem = memo(function MenuItem({
 	path,
 	label,
 	active,
-	mini,
 }: MenuItemProps) {
+	const { isOpen } = useToggleMenu();
 	return (
 		<Link
 			href={path}
 			className={clsx({
 				[styles.menuItem]: true,
 				[styles.menuItemActive]: active,
-				[styles.menuItemMini]: mini,
+				[styles.menuItemMini]: !isOpen,
 			})}
 		>
 			<Icon icon={icon} type={active ? 'fill' : 'outline'} />
