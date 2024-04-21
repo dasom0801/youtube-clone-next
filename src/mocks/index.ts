cdbd6ba6b92a1310d563d9b1cea7008da884dea1
Dasom <dev.dasom1012@gmail.com>
2024-04-21 12:39:59 +0900

    feat: mock server 설정

---
+++ src/mocks/index.ts
@@ -0,0 +1,15 @@
+export const startMockServer = () => {
+	if (process.env.NODE_ENV === 'development') {
+		if (typeof window === 'undefined') {
+			(async () => {
+				const { server } = await import('./node');
+				server.listen();
+			})();
+		} else {
+			(async () => {
+				const { worker } = await import('./browser');
+				worker.start();
+			})();
+		}
+	}
+};
